3dc94f49520e6877e2d1ba33581557d141903586
Asadullo <72871274+Asadullo2502@users.noreply.github.com>
2023-10-16 22:45:27 +0500

    remove solution from problem files

--- src/01-get-function-return-type/problem.ts
+++ src/01-get-function-return-type/problem.ts
@@ -1,12 +1,9 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
 const functionName = () => {
-  return "hello";
+  return "Hello";
 };
 
-/**
- * How do we extract FuncReturn from functionName?
- */
 type FuncReturn = unknown;
 
 type tests = [Expect<Equal<FuncReturn, string>>];
